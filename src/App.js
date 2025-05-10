0a44da76863000b222c964e9fb5e73d11a0112fb
Bartek5765 <bprzeczek@student.agh.edu.pl>
2025-05-10 11:00:05 +0200

    zmiany

--- src/App.js
+++ src/App.js
@@ -10,10 +10,20 @@ function App() {
         setEmail(event.target.value);
     }
 
+    let message = '';
+    if (email.length < 10) {
+        message = <div>Ale masz krótki adres!</div>
+    } else if (email.length <20) {
+        message = <div>wój adres e-mail jest w sam raz</div>
+    } else {
+        message = <div>wój adres e-mail jest stanowczo za długi</div>
+    }
+
   return (
       <div>
         <h1>System do zapisów na zajęcia</h1>
           <h2>Twój e-mail to {email}</h2>
+          <div>{message}</div>
           <input type="text" onChange={handleChange}/>
       </div>
   );
