283c5071bf9be31e1f51c8afbb555a7952f6ec63
Bartek5765 <bprzeczek@student.agh.edu.pl>
2025-05-10 11:52:30 +0200

    zmiany

--- src/App.js
+++ src/App.js
@@ -4,29 +4,42 @@ import {useState} from "react";
 
 function App() {
     const [email, setEmail] = useState('bprzeczek@agh.edu.pl');
+    const [authenticatedUsername, setAuthenticatedUsername] = useState(null);
 
 
     function handleChange(event) {
         setEmail(event.target.value);
     }
 
-    let message = '';
-    if (email.length < 10) {
-        message = <div>Ale masz krótki adres!</div>
-    } else if (email.length <20) {
-        message = <div>wój adres e-mail jest w sam raz</div>
-    } else {
-        message = <div>wój adres e-mail jest stanowczo za długi</div>
-    }
+    // let message = '';
+    // if (email.length < 10) {
+    //     message = <div>Ale masz krótki adres!</div>
+    // } else if (email.length <20) {
+    //     message = <div>wój adres e-mail jest w sam raz</div>
+    // } else {
+    //     message = <div>wój adres e-mail jest stanowczo za długi</div>
+    // }
 
-  return (
-      <div>
-        <h1>System do zapisów na zajęcia</h1>
-          <h2>Twój e-mail to {email}</h2>
-          <div>{message}</div>
-          <input type="text" onChange={handleChange}/>
-      </div>
-  );
+    return (
+        <div>
+            <h1>System do zapisów na zajęcia</h1>
+            {!authenticatedUsername && (
+                <div>
+                    Zaloguj się e-mailem
+                    <input type="text" onChange={handleChange}/>
+                    <button type="button" onClick={() => setAuthenticatedUsername(email)}>
+                        Wchodzę
+                    </button>
+                </div>
+            )}
+            {authenticatedUsername && (
+                <div>
+                    <h4>Jesteś zalogowany jako {authenticatedUsername}</h4>
+                    <a onClick={() => setAuthenticatedUsername(null)}>Wyloguj</a>
+                </div>
+            )}
+        </div>
+    );
 }
 
 
