8fe1c3a377574347c56072c556b36a3d26ada6ad
Bartek5765 <bprzeczek@student.agh.edu.pl>
2025-05-10 10:26:41 +0200

    zmiany

--- src/App.js
+++ src/App.js
@@ -1,48 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//
-//         <p>
-//           123
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-//
-// export default App;
+import {useState} from "react";
 
 function App() {
-    let email = 'bprzeczek@agh.edu.pl'
+    const [email, setEmail] = useState('bprzeczek@agh.edu.pl');
+
+
+    function handleChange(event) {
+        setEmail(event.target.value);
+    }
 
   return (
-
       <div>
-
         <h1>System do zapisów na zajęcia</h1>
           <h2>Twój e-mail to {email}</h2>
-
+          <input type="text" onChange={handleChange}/>
       </div>
-
   );
-
 }
 
 
